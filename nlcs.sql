9f16f4febc87207444eff8bb983858ae73d1d7cf
quoc187 <nguyentrongquocbk@gmail.com>
2021-03-20 15:34:43 +0700

    Fork from https://github.com/giangcse/htql

--- nlcs.sql
+++ nlcs.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 31, 2019 at 07:30 AM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.9
+-- Máy chủ: localhost:3307
+-- Thời gian đã tạo: Th3 20, 2021 lúc 09:31 AM
+-- Phiên bản máy phục vụ: 10.4.14-MariaDB-log
+-- Phiên bản PHP: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `nlcs`
+-- Cơ sở dữ liệu: `nlcs`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bill`
+-- Cấu trúc bảng cho bảng `bill`
 --
 
 CREATE TABLE `bill` (
@@ -36,19 +35,19 @@ CREATE TABLE `bill` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
 --
--- Dumping data for table `bill`
+-- Đang đổ dữ liệu cho bảng `bill`
 --
 
 INSERT INTO `bill` (`id`, `time`, `empaccount`, `total`) VALUES
-(0, '2019-10-30 07:10:47', 'giangphan', 156000),
-(1, '2019-10-30 07:10:01', 'giangphan', 457000),
-(2, '2019-10-30 07:10:13', 'giangphan', 952000),
-(3, '2019-10-31 10:10:16', 'giangphan', 58000);
+(0, '2019-10-30 07:10:47', 'quoc187', 156000),
+(1, '2019-10-30 07:10:01', 'quoc187', 457000),
+(2, '2019-10-30 07:10:13', 'quoc187', 952000),
+(3, '2019-10-31 10:10:16', 'quoc187', 58000);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `employees`
+-- Cấu trúc bảng cho bảng `employees`
 --
 
 CREATE TABLE `employees` (
@@ -65,25 +64,25 @@ CREATE TABLE `employees` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
 --
--- Dumping data for table `employees`
+-- Đang đổ dữ liệu cho bảng `employees`
 --
 
 INSERT INTO `employees` (`id`, `fullname`, `account`, `password`, `id_num`, `address`, `phone`, `job`, `start`, `user_group`) VALUES
-(1, 'Phan Thanh Giáº£ng', 'giangphan', 'a940d8b1b4dbed2f777656fd0d965759d99c8ea9', '331800117', 'Tam BÃ¬nh, VÄ©nh Long', '0868442808', 'Quáº£n lÃ½', '2018', 'admin'),
-(2, 'LÃ¢m VÄƒn Di', 'dilam', 'f502e82c25bba5a06cf68ffa87ecd02371c1a975', '123456789', 'ChÃ¢u ThÃ nh, KiÃªn Giang', '0859711330', 'Cháº¿ biáº¿n', '2018', ''),
-(3, 'Nguyá»…n PhÃºc Báº£o Khang', 'khangnguyen', 'c863bacbc71869fc68a88d11b1f8c6d3b2f42ca9', '987654321', 'Mang ThÃ­t, VÄ©nh Long', '0939964344', 'Phá»¥c vá»¥', '2018', NULL),
-(4, 'Tráº§n VÄƒn Khá»Ÿi', 'khoitran', 'cae1b52d0ab8b788a944a94be70dedffa876bd61', '123654987', 'Giá»“ng Riá»ng, KiÃªn Giang', '0392959702', 'Phá»¥c vá»¥', '2018', NULL),
-(5, 'Nguyá»…n ÄÃ¬nh KhÃ¢m', 'khamnguyen', '5f79460e4f906c045759e7bb5580b62aceb2cc85', '123456987', 'Long Má»¹, Háº­u Giang', '0356879141', 'Cháº¿ biáº¿n', '2018', NULL),
-(6, 'Nguyá»…n Minh Thuáº­t', 'thuatnguyen', '4aa49a57b3c11757e7e391f52347572bb837597f', '321456987', 'VÅ©ng LiÃªm, VÄ©nh Long', '0929090507', 'Phá»¥c vá»¥', '2018', NULL),
-(7, 'Há»“ KhÃ¡nh Duy', 'duyho', '00fb8183479aed9947aebded28b36c57de7eb16b', '321546789', 'Long Má»¹, Háº­u Giang', '0929262646', 'Phá»¥c vá»¥', '2018', NULL),
-(8, 'Tráº§n VÄ©nh TÆ°á»ng', 'tuongtran', 'e03a3b0df7ef2aa513131811010f32f66cfa8e06', '312645978', 'Ã” MÃ´n, Cáº§n ThÆ¡', '0907047617', 'Order', '2018', 'order'),
-(11, 'LÃª Thanh LÆ°Æ¡ng', 'luongle', 'aa93e733a85dcd3e134fca45d926427e19a05afb', '121345648', 'Káº¿ SÃ¡ch, SÃ³c TrÄƒng', '0824521572', 'Order', '2018', 'order'),
-(14, 'Tráº§n SÄ© Äáº¡t', 'dattran', 'fbd0b9c3677241899bad77be49dcbb69471a7ef5', '226444232', 'BÃ¬nh Minh, VÄ©nh Long', '0254545444', 'Cháº¿ biáº¿n', '2018', '');
+(1, 'Nguyễn Trọng Quốc', 'quoc187', '7c4a8d09ca3762af61e59520943dc26494f8941b', '331800117', 'Cau Giay, Ha Noi', '0868442808', 'Quản lý', '2018', 'admin'),
+(2, 'Hoàng Ngọc Huyền', 'dilam', 'f502e82c25bba5a06cf68ffa87ecd02371c1a975', '123456789', 'Cau Giay, Ha Noi', '0859711330', 'Chế biến', '2018', ''),
+(3, 'Nguyễn Trọng Khang', 'khangnguyen', 'c863bacbc71869fc68a88d11b1f8c6d3b2f42ca9', '987654321', 'Cau Giay, Ha Noi', '0939964344', 'Phục vụ', '2018', NULL),
+(4, 'Đỗ Minh Khôi', 'khoitran', 'cae1b52d0ab8b788a944a94be70dedffa876bd61', '123654987', 'Cau Giay, Ha Noi', '0392959702', 'Phục vụ', '2018', NULL),
+(5, 'Nguyễn Mạnh Khâm', 'khamnguyen', '5f79460e4f906c045759e7bb5580b62aceb2cc85', '123456987', 'Cau Giay, Ha Noi', '0356879141', 'Chế biến', '2018', NULL),
+(6, 'Nguyễn Minh Thuật', 'thuatnguyen', '4aa49a57b3c11757e7e391f52347572bb837597f', '321456987', 'Cau Giay, Ha Noi', '0929090507', 'Phục vụ', '2018', NULL),
+(7, 'Nguyễn Đức Duy', 'duyho', '00fb8183479aed9947aebded28b36c57de7eb16b', '321546789', 'Cau Giay, Ha Noi', '0929262646', 'Phục vụ', '2018', NULL),
+(8, 'Trần Văn Tường', 'tuongtran', 'e03a3b0df7ef2aa513131811010f32f66cfa8e06', '312645978', 'Cau Giay, Ha Noi', '0907047617', 'Order', '2018', 'order'),
+(11, 'Lê Thanh Lương', 'luongle', 'aa93e733a85dcd3e134fca45d926427e19a05afb', '121345648', 'Cau Giay, Ha Noi', '0824521572', 'Order', '2018', 'order'),
+(14, 'Trần Sơn Đạt', 'dattran', 'fbd0b9c3677241899bad77be49dcbb69471a7ef5', '226444232', 'Cau Giay, Ha Noi', '0254545444', 'Chế biến', '2018', '');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `product`
+-- Cấu trúc bảng cho bảng `product`
 --
 
 CREATE TABLE `product` (
@@ -95,58 +94,56 @@ CREATE TABLE `product` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
 --
--- Dumping data for table `product`
+-- Đang đổ dữ liệu cho bảng `product`
 --
 
 INSERT INTO `product` (`id`, `name`, `species`, `price`, `thumb_img`) VALUES
-(1, 'Americano', 'Cafe', 39000, 'https://product.hstatic.net/1000075078/product/americano_large.jpg'),
-(2, 'Báº¡c sá»‰u', 'Cafe', 29000, 'https://product.hstatic.net/1000075078/product/white_vnese_coffee_9968c1184d7f4634a9bb9fce7b5ff313_large.jpg'),
-(3, 'Cafe Ä‘en', 'Cafe', 29000, 'https://product.hstatic.net/1000075078/product/vnese_coffee_large.jpg'),
-(4, 'Cafe Sá»¯a', 'Cafe', 29000, 'https://product.hstatic.net/1000075078/product/cfs_large.jpg'),
-(5, 'Cappucinno', 'Cafe', 45000, 'https://product.hstatic.net/1000075078/product/cappuccino_large.jpg'),
-(6, 'Caramel Macchiato', 'Cafe', 55000, 'https://product.hstatic.net/1000075078/product/caramel_macchiato_b6f694f469794e12b04a91845f5fce2d_large.jpg'),
-(7, 'Cold brew cam sáº£', 'Cafe', 45000, 'https://product.hstatic.net/1000075078/product/colbrewcamsa_c2a45ec3fea94e248fd1b4f71abebe09_large.jpg'),
-(8, 'Cold brew phÃºc bá»“n tá»­', 'Cafe', 50000, 'https://product.hstatic.net/1000075078/product/coldbrew_raspberry_ac577fc483484f0cb4e35efe6bf14426_large.png'),
-(9, 'Cold brew sá»¯a tÆ°Æ¡i', 'Cafe', 50000, 'https://product.hstatic.net/1000075078/product/coldbrewmilk_09db086189ce43d5bb78172613af57dc_large.jpg'),
-(10, 'Cold brew sá»¯a tÆ°Æ¡i macchiatio', 'Cafe', 50000, 'https://product.hstatic.net/1000075078/product/coldbrewmacchiato_af7dc44ed71b4856bfe48a756ed939e6_large.jpg'),
-(11, 'Cold brew truyá»n thá»‘ng', 'Cafe', 45000, 'https://product.hstatic.net/1000075078/product/classic_47aa187278184a4ea36a59cf279350c0_large.jpg'),
-(12, 'Espresso', 'Cafe', 35000, 'https://product.hstatic.net/1000075078/product/espresso_large.jpg'),
-(13, 'Latte', 'Cafe', 45000, 'https://product.hstatic.net/1000075078/product/latte__1__large.jpg'),
-(16, 'Mocha', 'Cafe', 49000, 'https://product.hstatic.net/1000075078/product/mocha_large.jpg'),
-(17, 'Chocolate Ä‘Ã¡', 'Cafe', 55000, 'https://product.hstatic.net/1000075078/product/iced_chocolate_large.jpg'),
-(18, 'TrÃ  cherry macchiato', 'TrÃ  & Macchiato', 55000, 'https://product.hstatic.net/1000075078/product/cherry_mac_6a3403fdb832464da88de8c6e6ddf662_large.jpg'),
-(19, 'TrÃ  Ä‘Ã o cam sáº£', 'TrÃ  & Macchiato', 45000, 'https://product.hstatic.net/1000075078/product/tra_dao_cam_sa_on_bg_large.jpg'),
-(20, 'TrÃ  Ä‘en macchiato', 'TrÃ  & Macchiato', 42000, 'https://product.hstatic.net/1000075078/product/blacktea_macchiat_large.jpg'),
-(21, 'TrÃ  gáº¡o rang macchiato', 'TrÃ  & Macchiato', 48000, 'https://product.hstatic.net/1000075078/product/genmaicha_macchiatov_large.jpg'),
-(22, 'TrÃ  matcha latte', 'TrÃ  & Macchiato', 59000, 'https://product.hstatic.net/1000075078/product/matcha_latte_large.jpg'),
-(23, 'TrÃ  matcha macchiato', 'TrÃ  & Macchiato', 45000, 'https://product.hstatic.net/1000075078/product/matcha_macchiato_large.jpg'),
-(24, 'TrÃ  oolong sen An nhiÃªn', 'TrÃ  & Macchiato', 45000, 'https://product.hstatic.net/1000075078/product/trasen_29e9b22739f941ff84671361ae53f3dd_large.jpg'),
-(25, 'TrÃ  oolong váº£i NhÆ° Ã½', 'TrÃ  & Macchiato', 45000, 'https://product.hstatic.net/1000075078/product/travai_large.jpg'),
-(26, 'TrÃ  phÃºc bá»“n tá»­', 'TrÃ  & Macchiato', 49000, 'https://product.hstatic.net/1000075078/product/tea_raspberry_fda1ba90f506483eb6568f433690173a_large.png'),
-(27, 'TrÃ  xoÃ i macchiatio', 'TrÃ  & Macchiato', 55000, 'https://product.hstatic.net/1000075078/product/xoai_nong_mac_7f000cdac75e458e9ea5cdc3cb990419_large.jpg'),
-(28, 'TrÃ  xoÃ i macchiatio', 'TrÃ  & Macchiato', 55000, 'https://product.hstatic.net/1000075078/product/xoai_mac_c4534cc7607f4d58addf470a162e8359_large.jpg'),
-(29, 'Chanh sáº£ Ä‘Ã¡ xay', 'Thá»©c uá»‘ng Ä‘Ã¡ xay', 49000, 'https://product.hstatic.net/1000075078/product/chanhday_1x1_66281943c16b49d480a30860638fdc5e_large.jpg'),
-(30, 'Chocolate Ä‘Ã¡ xay', 'Thá»©c uá»‘ng Ä‘Ã¡ xay', 59000, 'https://product.hstatic.net/1000075078/product/chocolate_ice_blended_large.jpg'),
-(31, 'Cookies Ä‘Ã¡ xay', 'Thá»©c uá»‘ng Ä‘Ã¡ xay', 59000, 'https://product.hstatic.net/1000075078/product/cookies_ice_blended_large.jpg'),
-(32, 'ÄÃ o viá»‡t quáº¥t Ä‘Ã¡ xay', 'Thá»©c uá»‘ng Ä‘Ã¡ xay', 59000, 'https://product.hstatic.net/1000075078/product/dao_a0adb67ef806443298524236a4cc7e3c_large.jpg'),
-(33, 'Matcha Ä‘Ã¡ xay', 'Thá»©c uá»‘ng Ä‘Ã¡ xay', 59000, 'https://product.hstatic.net/1000075078/product/matcha_ice_blended_large.jpg'),
-(34, 'á»”i há»“ng viá»‡t quáº¥t Ä‘Ã¡ xay', 'Thá»©c uá»‘ng Ä‘Ã¡ xay', 59000, 'https://product.hstatic.net/1000075078/product/oihong_eba1ad20923841f6830e40e2dfd3e42a_large.jpg'),
-(35, 'PhÃºc bá»“n tá»­ cam Ä‘Ã¡ xay', 'Thá»©c uá»‘ng Ä‘Ã¡ xay', 59000, 'https://product.hstatic.net/1000075078/product/daxay_raspberry_01685c63649740678352a348f8b281a1_large.png'),
-(36, 'Sinh tá»‘ cam xoÃ i', 'Thá»©c uá»‘ng trÃ¡i cÃ¢y', 59000, 'https://product.hstatic.net/1000075078/product/mango_smoothie_large.jpg'),
-(37, 'Sinh tá»‘ viá»‡t quáº¥t', 'Thá»©c uá»‘ng trÃ¡i cÃ¢y', 59000, 'https://product.hstatic.net/1000075078/product/blueberry_smoothie_large.jpg'),
-(38, 'BÃ¡nh bÃ´ng lan trá»©ng muá»‘i', 'BÃ¡nh & snack', 29000, 'https://product.hstatic.net/1000075078/product/trungmui1_9abf7c43946b44e9948dbac1eff95e40_large.jpg'),
-(39, 'BÃ¡nh chocolate', 'BÃ¡nh & snack', 29000, 'https://product.hstatic.net/1000075078/product/choco_1x1_4faf8c80e6604cad88ce30528e2bd409_large.jpg'),
-(40, 'BÃ¡nh Croissant bÆ¡ tá»i', 'BÃ¡nh & snack', 29000, 'https://product.hstatic.net/1000075078/product/sungtrau_1x1_0e9a3e2590794cb9a6ff7ad9d9562c25_large.jpg'),
-(41, 'BÃ¡nh gáº¥u chocolate', 'BÃ¡nh & snack', 39000, 'https://product.hstatic.net/1000075078/product/gau_1x1_8b308db3a67a47688ff0d69a8c5615bf_large.jpg'),
-(42, 'BÃ¡nh matcha', 'BÃ¡nh & snack', 29000, 'https://product.hstatic.net/1000075078/product/matcha_178bdeeb1f9b47ea9f782048eb145f49_large.jpg'),
-(43, 'BÃ¡nh mÃ¬ chÃ  bÃ´ng phÃ´ mai', 'BÃ¡nh & snack', 32000, 'https://product.hstatic.net/1000075078/product/phomaichabong_1x1_e86c140c8a084458afcace64a93d2fd1_large.jpg'),
-(44, 'BÃ¡nh passion cheese', 'BÃ¡nh & snack', 29000, 'https://product.hstatic.net/1000075078/product/chanhday_1x1_da85397059a149e1b886237434f7ce48_large.jpg'),
-(45, 'BÃ¡nh tiramisu', 'BÃ¡nh & snack', 29000, 'https://product.hstatic.net/1000075078/product/tira_1x1_eb865751a24e4520be32a56129dd7a2d_large.jpg');
+(1, 'Americano', 'Cafe', 39000, 'https://i.ibb.co/qCdrfrx/americano.jpg'),
+(2, 'Bạc sửu', 'Cafe', 29000, 'https://i.ibb.co/68jX7RR/bacsiu.jpg'),
+(3, 'Cafe Đen', 'Cafe', 29000, 'https://i.ibb.co/2sWP63x/image.png'),
+(4, 'Cafe Sữa', 'Cafe', 29000, 'https://i.ibb.co/ZcnDDq7/image.png'),
+(5, 'Cappucinno', 'Cafe', 45000, 'https://i.ibb.co/C2ccwyG/capuchino.jpg'),
+(6, 'Caramel Macchiato', 'Cafe', 55000, 'https://i.ibb.co/DRr17Wj/image.png'),
+(7, 'Cold brew cam sả', 'Cafe', 45000, 'https://i.ibb.co/jkvS02Y/coldbrew-camsa.jpg'),
+(8, 'Cold brew phúc bồn tử', 'Cafe', 50000, 'https://i.ibb.co/q19ytJx/coldbrew-phuc-bon-tu.jpg'),
+(9, 'Cold brew sữa tươi', 'Cafe', 50000, 'https://i.ibb.co/C6rF7N0/coldbrew-sua-tuoi.jpg'),
+(10, 'Cold brew sữa tươi macchiatio', 'Cafe', 50000, 'https://i.ibb.co/JyNLPYt/image.png'),
+(11, 'Cold brew truyền thống', 'Cafe', 45000, 'https://i.ibb.co/Kbq9R8f/coldbrew-truyenthong.jpg'),
+(12, 'Espresso', 'Cafe', 35000, 'https://i.ibb.co/68XrBw3/espresso.jpg'),
+(13, 'Latte', 'Cafe', 45000, 'https://i.ibb.co/4mcT6Vb/latte.jpg'),
+(16, 'Mocha', 'Cafe', 49000, 'https://i.ibb.co/X454PBs/mocha.jpg'),
+(17, 'Chocolate Đá', 'Cafe', 55000, 'https://i.ibb.co/71cTDdV/chocolate-da-xay.jpg'),
+(18, 'Trà  cherry macchiato', 'Trà  & Macchiato', 55000, 'https://i.ibb.co/RSL5P0B/cherry-machiato.jpg'),
+(19, 'Trà đào cam sả', 'Trà  & Macchiato', 45000, 'https://i.ibb.co/vYBGL5h/tra-dao-cam-sa.jpg'),
+(20, 'Trà đen macchiato', 'Trà  & Macchiato', 42000, 'https://i.ibb.co/hmbDjhb/tra-den-machiato.jpg'),
+(21, 'Trà gạo rang macchiato', 'Trà  & Macchiato', 48000, 'https://product.hstatic.net/1000075078/product/genmaicha_macchiatov_large.jpg'),
+(22, 'Trà  matcha latte', 'Trà  & Macchiato', 59000, 'https://i.ibb.co/Wc4C30f/matcha-latte.jpg'),
+(23, 'Trà  matcha macchiato', 'Trà  & Macchiato', 45000, 'https://i.ibb.co/4dRpsdb/matcha-macchiato.jpg'),
+(24, 'Trà  oolong sen', 'Trà  & Macchiato', 45000, 'https://i.ibb.co/ZT9K0YW/olong-sen.jpg'),
+(25, 'Trà oolong vải như ý', 'Trà  & Macchiato', 45000, 'https://i.ibb.co/HGDXFWQ/olong-vai-nhu-y.jpg'),
+(26, 'Trà phúc bồn tử', 'Trà  & Macchiato', 49000, 'https://i.ibb.co/Bqq3KBn/tra-phuc-bon-tu.jpg'),
+(27, 'Trà xoài macchiato', 'Trà  & Macchiato', 55000, 'https://i.ibb.co/FVpSm0C/tra-xoai-macchiato.jpg'),
+(29, 'Chanh sả đá xay', 'Thức uống đá xay', 49000, 'https://i.ibb.co/cw6xRrj/chanh-sa-da-xay.jpg'),
+(30, 'Chocolate đá xay', 'Thức uống đá xay', 59000, 'https://i.ibb.co/71cTDdV/chocolate-da-xay.jpg'),
+(31, 'Cookies đá xay', 'Thức uống đá xay', 59000, 'https://i.ibb.co/54yrh8L/cookie-da-xay.jpg'),
+(32, 'Đào việt quất đá xay', 'Thức uống đá xay', 59000, 'https://i.ibb.co/wRng3rG/dao-viet-quat-xay.jpg'),
+(33, 'Matcha đá xay', 'Thức uống đá xay', 59000, 'https://i.ibb.co/Tq0mky0/matcha-da-xay.jpg'),
+(35, 'Phúc bồn tử cam đá xay', 'Thức uống đá xay', 59000, 'https://i.ibb.co/cC0LZKm/phuc-bon-tu-cam-xay.jpg'),
+(36, 'Sinh tố cam xoài', 'Thức uống trái cây', 59000, 'https://i.ibb.co/HGmthdF/sinh-to-cam-xoai.jpg'),
+(37, 'Sinh tố việt quất', 'Thức uống trái cây', 59000, 'https://i.ibb.co/w6zP6B1/sinh-to-viet-quat.jpg'),
+(38, 'Bánh bông lan trứng muối', 'Bánh & Snack', 29000, 'https://product.hstatic.net/1000075078/product/trungmui1_9abf7c43946b44e9948dbac1eff95e40_large.jpg'),
+(39, 'Bánh chocolate', 'Bánh & Snack', 29000, 'https://product.hstatic.net/1000075078/product/choco_1x1_4faf8c80e6604cad88ce30528e2bd409_large.jpg'),
+(40, 'Bánh Croissant', 'Bánh & Snack', 29000, 'https://product.hstatic.net/1000075078/product/sungtrau_1x1_0e9a3e2590794cb9a6ff7ad9d9562c25_large.jpg'),
+(41, 'Bánh gấu Chocolate', 'Bánh & Snack', 39000, 'https://product.hstatic.net/1000075078/product/gau_1x1_8b308db3a67a47688ff0d69a8c5615bf_large.jpg'),
+(42, 'Bánh matcha', 'Bánh & Snack', 29000, 'https://product.hstatic.net/1000075078/product/matcha_178bdeeb1f9b47ea9f782048eb145f49_large.jpg'),
+(43, 'Bánh mì chà bông phô mai', 'Bánh & Snack', 32000, 'https://i.ibb.co/vH19mV5/banh-pho-mai-tra-xanh.jpg'),
+(44, 'Bánh passion cheese', 'Bánh & Snack', 29000, 'https://product.hstatic.net/1000075078/product/chanhday_1x1_da85397059a149e1b886237434f7ce48_large.jpg'),
+(45, 'Bánh tiramisu', 'Bánh & Snack', 29000, 'https://product.hstatic.net/1000075078/product/tira_1x1_eb865751a24e4520be32a56129dd7a2d_large.jpg');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `shifts`
+-- Cấu trúc bảng cho bảng `shifts`
 --
 
 CREATE TABLE `shifts` (
@@ -158,7 +155,7 @@ CREATE TABLE `shifts` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
 --
--- Dumping data for table `shifts`
+-- Đang đổ dữ liệu cho bảng `shifts`
 --
 
 INSERT INTO `shifts` (`id`, `shiftName`, `dayOfShift`, `empAccount`, `salaryOfShift`) VALUES
@@ -250,7 +247,7 @@ INSERT INTO `shifts` (`id`, `shiftName`, `dayOfShift`, `empAccount`, `salaryOfSh
 -- --------------------------------------------------------
 
 --
--- Table structure for table `supplier`
+-- Cấu trúc bảng cho bảng `supplier`
 --
 
 CREATE TABLE `supplier` (
@@ -261,25 +258,25 @@ CREATE TABLE `supplier` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
 
 --
--- Dumping data for table `supplier`
+-- Đang đổ dữ liệu cho bảng `supplier`
 --
 
 INSERT INTO `supplier` (`id`, `name`, `phone`, `address`) VALUES
 (0, 'CÃ´ng ty ly nhá»±a BÃ¬nh Minh', '0934524014', 'Sá»‘ 68 Nguyá»…n Huá»‡, PhÆ°á»ng Báº¿n NghÃ©, Quáº­n 1, TP. HCM');
 
 --
--- Indexes for dumped tables
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `bill`
+-- Chỉ mục cho bảng `bill`
 --
 ALTER TABLE `bill`
   ADD PRIMARY KEY (`id`),
   ADD KEY `empaccount` (`empaccount`);
 
 --
--- Indexes for table `employees`
+-- Chỉ mục cho bảng `employees`
 --
 ALTER TABLE `employees`
   ADD PRIMARY KEY (`id`),
@@ -287,58 +284,58 @@ ALTER TABLE `employees`
   ADD KEY `account_2` (`account`);
 
 --
--- Indexes for table `product`
+-- Chỉ mục cho bảng `product`
 --
 ALTER TABLE `product`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `shifts`
+-- Chỉ mục cho bảng `shifts`
 --
 ALTER TABLE `shifts`
   ADD PRIMARY KEY (`id`),
   ADD KEY `empAccount` (`empAccount`);
 
 --
--- Indexes for table `supplier`
+-- Chỉ mục cho bảng `supplier`
 --
 ALTER TABLE `supplier`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT for table `employees`
+-- AUTO_INCREMENT cho bảng `employees`
 --
 ALTER TABLE `employees`
   MODIFY `id` int(4) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- AUTO_INCREMENT for table `product`
+-- AUTO_INCREMENT cho bảng `product`
 --
 ALTER TABLE `product`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
--- AUTO_INCREMENT for table `shifts`
+-- AUTO_INCREMENT cho bảng `shifts`
 --
 ALTER TABLE `shifts`
   MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=181;
 
 --
--- Constraints for dumped tables
+-- Các ràng buộc cho các bảng đã đổ
 --
 
 --
--- Constraints for table `bill`
+-- Các ràng buộc cho bảng `bill`
 --
 ALTER TABLE `bill`
   ADD CONSTRAINT `bill_ibfk_1` FOREIGN KEY (`empaccount`) REFERENCES `employees` (`account`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
--- Constraints for table `shifts`
+-- Các ràng buộc cho bảng `shifts`
 --
 ALTER TABLE `shifts`
   ADD CONSTRAINT `shifts_ibfk_1` FOREIGN KEY (`empAccount`) REFERENCES `employees` (`account`) ON DELETE NO ACTION ON UPDATE CASCADE;
